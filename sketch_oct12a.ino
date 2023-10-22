51fabe6a142a4a1a9c39fc14a377e3f63551ead5
Mobina14 <146319117+Mobina14@users.noreply.github.com>
2023-10-22 21:48:15 +0330

    Add files via upload

---
+++ sketch_oct12a.ino
@@ -0,0 +1,13 @@
+int yellow = 13;
+int green = 7;
+int red = 4;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
